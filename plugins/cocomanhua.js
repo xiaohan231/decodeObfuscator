aaf6737873b17d81f7a7eaaf04a7805868c05a37
Xwite <1797350009@qq.com>
2022-05-11 07:40:29 +0800

    feat: add common.js cocomanhua.js

---
+++ plugins/cocomanhua.js
@@ -0,0 +1,87 @@
+//自动解析cocomanhuan加密密钥
+//必须先解密
+globalThis.cocomanhua = () =>{
+    let map = new Map();
+    let keyNames = Array.of("C_DATA", "enc_code1", "enc_code2");
+    
+    //window.devtools.jsd(key, ...(window.C_DATA))
+    const extratKeys = {
+        CallExpression(path) {
+            const {
+                callee, arguments
+            } = path.node;
+            if (t.isMemberExpression(callee) && arguments.length === 2) {
+                let {
+                    object, property
+                } = callee;
+                if (!t.isMemberExpression(object)) return
+                if (//object.object.name === "window" &&
+                   object.property.name === "devtools" &&
+                   property.name === "jsd"
+               ) {
+    
+                    let[key, tar] = arguments;
+                    if (!t.isMemberExpression(tar.callee)) return;
+                    let key_name = tar.callee.object.arguments[0].property.name;
+                    if(!keyNames.find(name=>name==key_name)) return;
+                    let _set = map.get(key_name) || new Set();
+                    console.info(path.toString());
+                    console.log(`=== find aes key of ${key_name} ===`);
+                    let decodeKey = getBindingValue(key, path);
+                    _set.add(decodeKey);
+                    map.set(key_name, _set)
+    
+                }
+            }
+        }
+    }
+    
+    // ....(我是密钥, ...C_DATA...)
+    const extratKeys2 = {
+        CallExpression(path) {
+            const { arguments } = path.node;
+            if (arguments.length === 2) {
+               let[key, tar] = arguments;
+               let code = generator(tar).code;
+               let key_name= keyNames.find(name=>code.includes(name));
+               if (key_name) {
+                    let _set = map.get(key_name) || new Set();
+                    console.info(path.toString())
+                    let decodeKey = getBindingValue(key, path);
+                    _set.add(decodeKey);
+                    map.set(key_name, _set)
+                }
+    
+            }
+        }
+    }
+    
+    
+    console.info('extra cocomanahua keys...\n');
+    
+    let visitors = Array.of(extratKeys, extratKeys2);
+    
+    for (visitor of visitors) {
+        traverse(ast, visitor);
+        if(Array.from(map.keys()).sort().toString() != keyNames.sort().toString()) {
+            console.warn(`keys of keysMap not equal to [ ${keyNames} ], switch to another visitor`)
+        } else { break }
+    }
+    
+    for ([key, value] of map) {
+        map.set(key, Array.from(value))
+    }
+    
+    let keys = JSON.stringify(Object.fromEntries(map));
+    
+    if (typeof require == "function") {
+        let keysFile = "./coco_keys.json";
+        const fs = require('fs');
+        fs.writeFile(keysFile, keys, (err) => {});
+        console.info(`coco漫画密钥文件: ${keysFile}`);
+    } else {
+        globalThis.keys = keys;
+        console.info("密钥保存到keys变量")
+    }
+    console.log(keys);
+}
---
+++ plugins/common.js
@@ -0,0 +1,74 @@
+//ast通用插件使用示范
+globalThis.common = () => {
+    //字面量解混淆
+    console.info("traverse Hex or Unicode String.......");
+    
+    traverse(ast, simplifyLiteral);
+    
+    console.info("constantFold.......");
+    
+    traverse(ast, constantFold);
+    
+    console.info("delete Repeat Define.......");
+    
+    traverse(ast, deleteRepeatDefine);
+    
+    traverse(ast, SimplifyIfStatement);
+    
+    traverse(ast, standardLoop);
+    
+    console.info("resolve Sequence.......");
+    
+    traverse(ast, resolveSequence);
+    
+    console.info("traverse CallExpress To ToLiteral.......");
+    
+    traverse(ast, CallExpressToLiteral);
+    
+    console.info("constantFold.......");
+    
+    traverse(ast, constantFold);
+    
+    
+    //object key值Literal
+    console.info("Object Preconditioning .......");
+    
+    traverse(ast, keyToLiteral);
+    
+    traverse(ast, preDecodeObject);
+    
+    //处理object
+    
+    console.info("Object Decode .......\n");
+    
+    traverse(ast, decodeObject);
+    
+    console.info("Control Flow Decoding.......");
+    
+    traverse(ast, decodeControlFlow);
+    
+    console.info("constantFold.......");
+    
+    traverse(ast, constantFold);
+    
+    console.info("remove Dead Code.......");
+    
+    traverse(ast, removeDeadCode);
+    
+    ast = parser.parse(generator(ast).code);
+    
+    traverse(ast, removeDeadCode);
+    
+    console.info("simplifyLiteral.......");
+    
+    traverse(ast, simplifyLiteral);
+    
+    console.info("deleteObfuscatorCode.......");
+    
+    //可能会误删一些代码，可屏蔽
+    traverse(ast, deleteObfuscatorCode);
+    
+    console.info("FormatMember...");
+    
+    traverse(ast, FormatMember);
+}
