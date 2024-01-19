c4eb5ad75428ed2593baa709fd94559309d45a13
xiaohan231 <122842749+xiaohan231@users.noreply.github.com>
2024-01-20 05:41:40 +0800

    Update colamanhua.js

--- plugins/colamanhua.js
+++ plugins/colamanhua.js
@@ -3,46 +3,46 @@
  cola正文图片aes解密密钥解析
 */
 globalThis.colamanhua = () => {
-
   //解ob混淆
   load('common');
   //evalPacker混淆
   load('evalPacker');
 
   const extractedCode = [];
+  let deleteFlag = false;
 
-  // 提取解混淆的代码片段并添加到ImgKey函数中
+  // 提取解混淆的代码片段
   const extractCode = {
     IfStatement(path) {
       const { test, consequent } = path.node;
-      if (
-        test.type === 'BinaryExpression' &&
-        test.operator === '==' &&
-        test.left.name === 'G' &&
-        consequent.length === 1 &&
-        consequent[0].type === 'ExpressionStatement' &&
-        consequent[0].expression.type === 'AssignmentExpression' &&
-        consequent[0].expression.operator === '=' &&
-        consequent[0].expression.left.name === 'I'
-      ) {
-        const value = consequent[0].expression.right.value;
-        const extractedCodeSnippet = `if (G === "${test.right.value}") { I = "${value}"; }`;
-        extractedCode.push(extractedCodeSnippet);
-        path.remove();
+      if (test.type === 'BinaryExpression' &&
+          test.operator === '==' &&
+          test.left.name === 'G' &&
+          consequent.length === 1 &&
+          consequent[0].type === 'BlockStatement') {
+        if (consequent[0].body.length > 0) {
+          deleteFlag = true;
+          const startIndex = consequent[0].body.findIndex(node => node.type === 'VariableDeclaration' && node.declarations[0].id.name === 'I');
+          if (startIndex !== -1) {
+            consequent[0].body.splice(0, startIndex + 1);
+          }
+          const endIndex = consequent[0].body.findIndex(node => node.type === 'ExpressionStatement' && node.expression.left.property.name === 'I');
+          if (endIndex !== -1) {
+            consequent[0].body.splice(endIndex);
+          }
+          extractedCode.push(...consequent[0].body.map(node => generator(node).code));
+        }
       }
     }
   }
 
   traverse(ast, extractCode);
 
-  // 构建 ImgKey 函数
+  // 构建完整的ImgKey函数
   const imgKeyFunction = `
 function ImgKey(G) {
   var H = "";
-  if (G != "" && G != "0") {
-    ${extractedCode.join('\n')}
-    H = I;
-  }
+  ${deleteFlag ? extractedCode.join('\n') : ''}
   return H;
 }
 `;
@@ -50,14 +50,12 @@ function ImgKey(G) {
   let keys = imgKeyFunction;
 
   if (typeof require == "function") {
-    let keysFile = "./cola_keys.js";
     const fs = require('fs');
-    fs.writeFile(keysFile, keys, () => {});
-    console.info(`cola漫画密钥文件: ${keysFile}`);
+    fs.writeFile('./cola_keys.js', keys, () => {});
+    console.info('cola漫画密钥文件已保存为cola_keys.js');
   } else {
     globalThis.keys = keys;
-    console.info("密钥保存到keys变量");
+    console.info('密钥已保存到keys变量');
   }
   console.log(keys);
-
 }
